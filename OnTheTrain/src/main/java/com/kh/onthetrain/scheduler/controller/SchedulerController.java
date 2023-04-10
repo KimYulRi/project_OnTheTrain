4973a1dd446876d28d40d91f4661db704b2fa723
이정준 <ljj0210@gmail.com>
2023-04-10 16:47:53 +0900

    FIR-36

--- OnTheTrain/src/main/java/com/kh/onthetrain/scheduler/controller/SchedulerController.java
+++ OnTheTrain/src/main/java/com/kh/onthetrain/scheduler/controller/SchedulerController.java
@@ -1,8 +1,13 @@
 package com.kh.onthetrain.scheduler.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.kh.onthetrain.scheduler.model.entity.Scheduler;
+import com.kh.onthetrain.scheduler.service.SchedulerService;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -11,7 +16,9 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/scheduler")
 public class SchedulerController {
 
-	
+	@Autowired
+	private SchedulerService service;
+
 	@GetMapping("/main")
 	// 스케줄러 메인으로 보내는 메소드
 	public String toMain() {
@@ -24,6 +31,15 @@ public class SchedulerController {
 	public String toCreate() {
 		log.info("toCreate() 메소드 실행");
 		return "scheduler/schedulerCreate";
+	}
+
+	@GetMapping("/view")
+	// 스케줄러 하나를 조회하는 메소드
+	public Scheduler schedluerView(@RequestParam int no) {
+		log.info("schedluerView() 메소드 실행");
+		Scheduler sc = service.findSchedulerByNo(no);
+		
+		return sc;
 	}
 
 }
