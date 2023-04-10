432cd18b2e973958219d9e3fb98163517fb3d135
이정준 <ljj0210@gmail.com>
2023-04-10 13:42:54 +0900

    로그 포함

--- OnTheTrain/src/main/java/com/kh/onthetrain/scheduler/controller/SchedulerController.java
+++ OnTheTrain/src/main/java/com/kh/onthetrain/scheduler/controller/SchedulerController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 @RequestMapping("/scheduler")
 public class SchedulerController {
@@ -12,14 +15,14 @@ public class SchedulerController {
 	@GetMapping("/main")
 	// 스케줄러 메인으로 보내는 메소드
 	public String toMain() {
-
+		log.info("toMain() 메소드 실행");
 		return "scheduler/schedulerMain";
 	}
 
 	@GetMapping("/create")
 	// 스케줄러 생성으로 보내는 메소드
 	public String toCreate() {
-
+		log.info("toCreate() 메소드 실행");
 		return "scheduler/schedulerCreate";
 	}
 
