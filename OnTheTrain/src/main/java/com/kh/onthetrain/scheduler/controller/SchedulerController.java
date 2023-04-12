fa762dfa22145ea888b1e3da512643e685536f8d
이정준 <ljj0210@gmail.com>
2023-04-12 17:56:35 +0900

    스케줄러 생성 페이지 컨트롤러 및 js 구현 중

--- OnTheTrain/src/main/java/com/kh/onthetrain/scheduler/controller/SchedulerController.java
+++ OnTheTrain/src/main/java/com/kh/onthetrain/scheduler/controller/SchedulerController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.kh.onthetrain.scheduler.model.entity.Scheduler;
 import com.kh.onthetrain.scheduler.service.SchedulerService;
@@ -26,13 +27,42 @@ public class SchedulerController {
 		return "scheduler/schedulerMain";
 	}
 
-	@GetMapping("/create")
-	// 스케줄러 생성으로 보내는 메소드
-	public String toCreate() {
-		log.info("toCreate() 메소드 실행");
-		return "scheduler/schedulerCreate";
+	// 스케줄러 생성 메인(일정 생성)으로 보내는 메소드
+	@RequestMapping(value= {"/create", "/create/event"})
+	public ModelAndView toCreate(ModelAndView modelAndView) {
+		log.info("toCreate/CreatEvent() 메소드 실행");
+		
+		// 기본으로 출력되는 스케줄러 요소는 '일정'
+		modelAndView.addObject("currentComponent", "일정");
+		modelAndView.setViewName("scheduler/schedulerCreate");
+		
+		return modelAndView;
+	}
+	
+	// 숙소 생성으로 보내는 메소드
+	@RequestMapping("/create/accommodation")
+	public ModelAndView toCreateAccommodation(ModelAndView modelAndView) {
+		log.info("toCreateAccommodation() 메소드 실행");
+	
+		// 현재 요소를 '숙소'로 변경
+		modelAndView.addObject("currentComponent", "숙소");
+		modelAndView.setViewName("scheduler/schedulerCreate");
+		
+		return modelAndView;
 	}
 
+	// 승차권 생성으로 보내는 메소드
+	@RequestMapping("/create/ticket")
+	public ModelAndView toCreateTicket(ModelAndView modelAndView) {
+		log.info("toCreateTicket() 메소드 실행");
+	
+		// 현재 요소를 '승차권'으로 변경
+		modelAndView.addObject("currentComponent", "승차권");
+		modelAndView.setViewName("scheduler/schedulerCreate");
+		
+		return modelAndView;
+	}
+	
 	@GetMapping("/view")
 	// 스케줄러 하나를 조회하는 메소드
 	public Scheduler schedluerView(@RequestParam int no) {
