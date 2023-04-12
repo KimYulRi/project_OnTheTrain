04dfca03ec9966b98dbd7d222c1134fb7675e2c3
이정준 <ljj0210@gmail.com>
2023-04-12 16:03:48 +0900

    스케줄러 요소 기본 매핑(selectByNo) 완료

--- OnTheTrain/src/main/java/com/kh/onthetrain/scheduler/service/SchedulerService.java
+++ OnTheTrain/src/main/java/com/kh/onthetrain/scheduler/service/SchedulerService.java
@@ -1,9 +1,21 @@
 package com.kh.onthetrain.scheduler.service;
 
 import com.kh.onthetrain.scheduler.model.entity.Scheduler;
+import com.kh.onthetrain.scheduler.model.entity.SchedulerAccommodation;
+import com.kh.onthetrain.scheduler.model.entity.SchedulerEvent;
+import com.kh.onthetrain.scheduler.model.entity.SchedulerImage;
+import com.kh.onthetrain.scheduler.model.entity.SchedulerTicket;
 
 public interface SchedulerService {
 
 	Scheduler findSchedulerByNo(int no);
+
+	SchedulerAccommodation findSchedulerAccommodationByNo(int no);
+
+	SchedulerEvent findSchedulerEventByNo(int no);
+
+	SchedulerTicket findSchedulerTicketByNo(int no);
+
+	SchedulerImage findSchedulerImageByNo(int no);
 
 }
