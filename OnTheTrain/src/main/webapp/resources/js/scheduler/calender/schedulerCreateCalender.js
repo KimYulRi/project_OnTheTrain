58df652b535e489132f0a3400b58641963671bf0
ljj0210 <ljj0210@gmail.com>
2023-04-16 22:31:18 +0900

    FIR-52

---
+++ OnTheTrain/src/main/webapp/resources/js/scheduler/calender/schedulerCreateCalender.js
@@ -0,0 +1,94 @@
+/* calender */
+
+const dp = new DayPilot.Calendar("dp", {
+  theme: "onthetrain_theme",
+  viewType: "Week",
+  startDate: "2022-03-21",
+  columnWidth: 200,
+  eventDeleteHandling: "Update",
+  headerHeight: 80,
+  headerToolbar: [
+    {
+      start: "2022-03-21",
+      end: "2022-03-27",
+      cells: [
+        { text: "Monday", cssClass: "header-day" },
+        { text: "Tuesday", cssClass: "header-day" },
+        { text: "Wednesday", cssClass: "header-day" },
+        { text: "Thursday", cssClass: "header-day" },
+        { text: "Friday", cssClass: "header-day" },
+        { text: "Saturday", cssClass: "header-day" },
+        { text: "Sunday", cssClass: "header-day" },
+      ],
+    },
+  ],
+
+  onEventClick: async (args) => {
+    const colors = [
+      { name: "Blue", id: "#3c78d8" },
+      { name: "Green", id: "#6aa84f" },
+      { name: "Yellow", id: "#f1c232" },
+      { name: "Red", id: "#cc0000" },
+    ];
+
+    const form = [
+      { name: "Text", id: "text" },
+      { name: "Start", id: "start", type: "datetime" },
+      { name: "End", id: "end", type: "datetime" },
+      { name: "Color", id: "barColor", type: "select", options: colors },
+    ];
+
+    const modal = await DayPilot.Modal.form(form, args.e.data);
+
+    if (modal.canceled) {
+      return;
+    }
+
+    dp.events.update(modal.result);
+  },
+  onBeforeEventRender: (args) => {
+    args.data.barBackColor = "transparent";
+    if (!args.data.barColor) {
+      args.data.barColor = "#333";
+    }
+  },
+  onTimeRangeSelected: async (args) => {
+    const form = [{ name: "Name", id: "text" }];
+
+    const data = {
+      text: "Event",
+    };
+
+    const modal = await DayPilot.Modal.form(form, data);
+    dp.clearSelection();
+
+    if (modal.canceled) {
+      return;
+    }
+
+    dp.events.add({
+      start: args.start,
+      end: args.end,
+      id: DayPilot.guid(),
+      text: modal.result.text,
+      barColor: "#3c78d8",
+    });
+  },
+  onHeaderClick: (args) => {
+    console.log("args", args);
+  },
+});
+
+dp.init();
+
+const events = [
+  {
+    start: "2022-03-21T11:00:00",
+    end: "2022-03-21T14:00:00",
+    id: 1,
+    text: "Event 1",
+    barColor: "#3c78d8",
+  },
+];
+dp.update({ events });
+
--- OnTheTrain/src/main/webapp/resources/js/scheduler/schedulerCreate.js
+++ OnTheTrain/src/main/webapp/resources/js/scheduler/schedulerCreate.js
@@ -21,6 +21,7 @@ $("select[name='locationCode']").on("change", function () {
         // 반환값이 빈 객체리스트인 경우
         // 결과가 없음을 알리는 영역 표시
         $("#eventList").empty();
+        $("#eventList").css("min-height", "unset");
         $("#noResultArea").show();
         return;
       }
@@ -124,104 +125,3 @@ addCardButton.addEventListener("click", () => {
 
   waitComponentList.appendChild(newCard);
 });
-
-/* calender */
-
-const dp = new DayPilot.Calendar("dp", {
-  theme: "onthetrain_theme",
-  viewType: "Week",
-  startDate: "2022-03-21",
-
-  columnWidth: 200,
-  eventDeleteHandling: "Update",
-  headerDateFormat: "dddd",
-
-  onEventClick: async (args) => {
-    const colors = [
-      { name: "Blue", id: "#3c78d8" },
-      { name: "Green", id: "#6aa84f" },
-      { name: "Yellow", id: "#f1c232" },
-      { name: "Red", id: "#cc0000" },
-    ];
-
-    const form = [
-      { name: "Text", id: "text" },
-      { name: "Start", id: "start", type: "datetime" },
-      { name: "End", id: "end", type: "datetime" },
-      { name: "Color", id: "barColor", type: "select", options: colors },
-    ];
-
-    const modal = await DayPilot.Modal.form(form, args.e.data);
-
-    if (modal.canceled) {
-      return;
-    }
-
-    dp.events.update(modal.result);
-  },
-  onBeforeEventRender: (args) => {
-    args.data.barBackColor = "transparent";
-    if (!args.data.barColor) {
-      args.data.barColor = "#333";
-    }
-  },
-  onTimeRangeSelected: async (args) => {
-    const form = [{ name: "Name", id: "text" }];
-
-    const data = {
-      text: "Event",
-    };
-
-    const modal = await DayPilot.Modal.form(form, data);
-    dp.clearSelection();
-
-    if (modal.canceled) {
-      return;
-    }
-
-    dp.events.add({
-      start: args.start,
-      end: args.end,
-      id: DayPilot.guid(),
-      text: modal.result.text,
-      barColor: "#3c78d8",
-    });
-  },
-  onHeaderClick: (args) => {
-    console.log("args", args);
-  },
-});
-
-dp.init();
-
-const events = [
-  {
-    start: "2022-03-21T11:00:00",
-    end: "2022-03-21T14:00:00",
-    id: 1,
-    text: "Event 1",
-    barColor: "#3c78d8",
-  },
-  {
-    start: "2022-03-22T12:00:00",
-    end: "2022-03-22T15:00:00",
-    id: 2,
-    text: "Event 2",
-    barColor: "#6aa84f",
-  },
-  {
-    start: "2022-03-23T10:00:00",
-    end: "2022-03-23T15:00:00",
-    id: 3,
-    text: "Event 3",
-    barColor: "#f1c232",
-  },
-  {
-    start: "2022-03-24T12:00:00",
-    end: "2022-03-24T16:00:00",
-    id: 4,
-    text: "Event 4",
-    barColor: "#cc0000",
-  },
-];
-dp.update({ events });
